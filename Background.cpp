d7b73574350858691dc9e0123fbb15b372fe2a1c
danharker20 <33772174+danharker20@users.noreply.github.com>
2017-12-06 19:32:39 -0800

    Update Background.cpp

--- Background.cpp
+++ Background.cpp
@@ -10,13 +10,15 @@ Background::~Background()
 {
 }
 
+//load background
+//puts tiles on screen
 void Background::loadBG(std::string path, int sizeX, int sizeY)
 {
 	char tile;
 	std::fstream mapFile;
 	mapFile.open(path);
 
-		
+	//loop that reads file and puts tiles on accordingly 
 	for (int y = 0; y < sizeY; y++)
 	{
 		for (int x = 0; x < sizeX; x++)
